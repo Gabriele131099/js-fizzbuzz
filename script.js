0d71012820ed77c460cd6ad3859106a8365b4470
gabriele131099 <gabrisanna131099@gmail.com>
2021-06-01 14:37:54 +0200

    struttura

--- script.js
+++ script.js
@@ -6,5 +6,22 @@ var fizzbuzz = "FizzBuzz"
 
 for (var i = 0; i <= 100; i++) {
         array.push(i)
+        if (array[i]%3 === 0) {
+            array[i]= fizz
+        } else 
+        { 
+            if (array[i]%5===0) {
+                array[i]= buzz
+
+            } else {
+                if (array[i]%3 === 0 && array[i]%5===0 ) {
+                    array[i]= fizzbuzz
+ 
+                }
+ 
+            }
+            
+        }
 }
 stampa.innerHTML=  array
+
