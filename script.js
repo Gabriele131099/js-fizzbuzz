29974a79c60642238215444689df079404292344
gabriele131099 <gabrisanna131099@gmail.com>
2021-06-01 15:54:31 +0200

    struttura finita

--- script.js
+++ script.js
@@ -3,8 +3,10 @@ var stampa= document.getElementById("stampa")
 var fizz = "Fizz"
 var buzz = "Buzz"
 var fizzbuzz = "FizzBuzz"
+
+
 for (var i = 0; i < 100; i++) {  //se metto 1 a i non funziona  !important
-    array.push(i)
+    array.push(i+1)
         if (array[i]%3 === 0 && array[i]%5 === 0 ) {
             array[i]= fizzbuzz
         }
@@ -17,5 +19,27 @@ for (var i = 0; i < 100; i++) {  //se metto 1 a i non funziona  !important
                 } 
         }
 }
-stampa.innerHTML=  array
 
+stampa.innerHTML= array;
+
+for(var i = 1; i < 100; i++){
+
+    if (i%3 === 0 && i%5 === 0) {
+
+        document.write(fizzbuzz + '<br>');
+        
+    }
+    if (i%3  ===  0) {
+        document.write(fizz + '<br>');
+
+    } else { if (i%5 === 0) {
+        document.write(buzz + '<br>');
+
+        
+    }else{
+        document.write(i + '<br>');
+
+    }
+        
+    }
+}
