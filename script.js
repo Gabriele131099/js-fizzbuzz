645c1b9a323e6eaf9c8b0d1f29ad07628393b711
gabriele131099 <gabrisanna131099@gmail.com>
2021-06-01 15:11:41 +0200

    struttura

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var fizz = "Fizz"
 var buzz = "Buzz"
 var fizzbuzz = "FizzBuzz"
 
-for (var i = 0; i <= 100; i++) {  //se metto 1 a i non funziona  !important
+for (var i = 0; i < 100; i++) {  //se metto 1 a i non funziona  !important
     array.push(i)
         if (array[i]%3 === 0 && array[i]%5 === 0 ) {
             array[i]= fizzbuzz
