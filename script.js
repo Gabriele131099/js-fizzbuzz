34768c74b94b89c1938cbd5259735f8491519c98
gabriele131099 <gabrisanna131099@gmail.com>
2021-06-01 15:19:56 +0200

    struttura

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ var stampa= document.getElementById("stampa")
 var fizz = "Fizz"
 var buzz = "Buzz"
 var fizzbuzz = "FizzBuzz"
-
 for (var i = 0; i < 100; i++) {  //se metto 1 a i non funziona  !important
     array.push(i)
         if (array[i]%3 === 0 && array[i]%5 === 0 ) {
